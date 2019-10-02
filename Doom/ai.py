27e487539e1a05ef8307973a78f8ccae449098ef
RiviereGregory <gregory.riviere@laposte.net>
2019-10-02 20:05:43 +0200

    Ajout Doom etape 2

---
+++ Doom/ai.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# AI for Doom
+
+
+
+# Importing the libraries
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+# Importing the packages for OpenAI and Doom
+import gym
+from gym import wrappers
+import vizdoomgym
+
+# Importing the other Python files
+import experience_replay, image_preprocessing
+
+
+
+# Part 1 - Building the AI
+
+
+# Part 2 - Training the AI with Deep Convolutional Q-Learning
+
