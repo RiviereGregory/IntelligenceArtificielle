d996b871120a62cf9472226bfa9734f45555eac2
RiviereGregory <gregory.riviere@laposte.net>
2019-10-02 20:32:37 +0200

    Ajout Doom ajout CNN

--- Doom/ai.py
+++ Doom/ai.py
@@ -24,6 +24,31 @@ import experience_replay, image_preprocessing
 
 # Part 1 - Building the AI
 
+class CNN(nn.Module):
+    
+    def __init__(self, number_actions):
+        super(CNN, self).__init__()
+        self.convolution1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5)
+        self.convolution2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
+        self.convolution3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=2)
+        self.fc1 = nn.Linear(in_features=self.count_neurons((1, 80, 80)), out_features=40)
+        self.fc2 = nn.Linear(in_features=40, out_features=number_actions)
+        
+    def count_neurons(self, image_dim):
+        x = Variable(torch.rand(1, *image_dim))
+        x = F.relu(F.max_pool2d(self.convolution1(x), kernel_size=3, stride=2))
+        x = F.relu(F.max_pool2d(self.convolution2(x), kernel_size=3, stride=2))
+        x = F.relu(F.max_pool2d(self.convolution3(x), kernel_size=3, stride=2))
+        return x.data.view(1, -1).size(1)
+        
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.convolution1(x), kernel_size=3, stride=2))
+        x = F.relu(F.max_pool2d(self.convolution2(x), kernel_size=3, stride=2))
+        x = F.relu(F.max_pool2d(self.convolution3(x), kernel_size=3, stride=2)) 
+        x = x.view(x.size(0), -1) # Flatening
+        x = F.relu(self.fc1(x)) # Fonction d'activation
+        x = self.fc2(x)
+        return x
 
 # Part 2 - Training the AI with Deep Convolutional Q-Learning
 
